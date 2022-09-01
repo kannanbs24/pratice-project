0a319da9cbfc6cadd00590033900186ed91eb2fc
kannan <kannanbsk1609080@gmail.com>
2022-09-01 20:26:34 +0530

    added

---
+++ pratice-projects/pratice-projects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace pratice_projects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = File.ReadAllText("E:\\project-pratice\\pratice-project\\studentdata.txt");
+            Console.WriteLine(data);
+        }
+    }
+}
